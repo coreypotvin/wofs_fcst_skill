c8935d5c20e7f71f91020be95c8a6a45e70bb79e
Corey <corey.potvin@noaa.gov>
2023-02-15 13:13:47 -0600

    update readme

---
+++ wofs_fcst_skill/imports.py
@@ -0,0 +1,37 @@
+import warnings
+warnings.simplefilter("ignore", category=DeprecationWarning)
+
+import sys, time, itertools, pickle, copy
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as P
+from scipy import mean
+from numpy.random import uniform
+from math import sqrt
+import random
+from scipy.stats import gmean, randint as sp_randint
+from joblib import dump, load
+from pandas.api.types import is_numeric_dtype
+from subprocess import check_call
+
+from xgboost import XGBClassifier
+from skopt import BayesSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier, StackingClassifier, HistGradientBoostingClassifier
+from sklearn.metrics import make_scorer, roc_auc_score, accuracy_score, balanced_accuracy_score, confusion_matrix, log_loss, roc_curve, auc, matthews_corrcoef, hinge_loss
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.neural_network import MLPClassifier
+from sklearn.utils import resample
+from sklearn.inspection import permutation_importance
+from sklearn.tree import export_graphviz
+
+from ordinal import OrdinalClassifier
+#import mord
+import skexplain
+from skexplain.main.PermutationImportance import metrics
+from ml_workflow.preprocess.preprocess import CorrelationFilter
+
